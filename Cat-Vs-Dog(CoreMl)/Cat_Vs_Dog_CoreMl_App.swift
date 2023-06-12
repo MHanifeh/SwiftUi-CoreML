8daa5d9d123fe65f48db92e1f9db428a2dbf4bb4
MHanifeh <hanifehmahdi3@gmail.com>
2023-06-13 01:02:42 +0330

    Initial Commit

---
+++ Cat-Vs-Dog(CoreMl)/Cat_Vs_Dog_CoreMl_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Vs_Dog_CoreMl_App.swift
+//  Cat-Vs-Dog(CoreMl)
+//
+//  Created by MahdiHanifeh on 3/23/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct Cat_Vs_Dog_CoreMl_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
